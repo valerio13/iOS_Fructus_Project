102b224140efe58131a79afa289fac0c0d112d0d
Valerio <valerio.colantonio@sngular.com>
2021-10-28 10:06:12 +0200

    Created custom components

---
+++ Fructus/View/FruitCardView.swift
@@ -0,0 +1,49 @@
+//
+//  FruitCardView.swift
+//  Fructus
+//
+//  Created by Valerio Colantonio on 27/10/21.
+//
+
+import SwiftUI
+
+struct FruitCardView: View {
+    //MARK: PROPERTIES
+    
+    //MARK: BODY
+    var body: some View {
+        ZStack {
+            VStack(spacing: 20) {
+                Image("blueberry")
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
+                
+                Text("Blueberry")
+                    .foregroundColor(Color.white)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 2, x: 2, y: 2)
+                
+                Text("Blueberries are sweet, nutritius and wildly popular fruit all over the world.")
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 16)
+                    .frame(maxWidth: 480)
+                
+                StartButtonView()
+            }
+        }
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+        .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"),Color("ColorBlueberryDark")]), startPoint: .top, endPoint: .bottom))
+        .cornerRadius(20)
+    }
+}
+
+//MARK: PREVIEW
+struct FruitCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitCardView()
+            .previewLayout(.fixed(width: 320, height: 640))
+    }
+}
