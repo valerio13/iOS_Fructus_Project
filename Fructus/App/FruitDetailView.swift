7c7bc80cf248a55b57d53670f4ea00c772596563
Valerio <valerio.colantonio@sngular.com>
2021-11-05 14:52:34 +0100

    Added detail view

---
+++ Fructus/App/FruitDetailView.swift
@@ -0,0 +1,66 @@
+//
+//  FruitDetailView.swift
+//  Fructus
+//
+//  Created by Valerio Colantonio on 4/11/21.
+//
+
+import SwiftUI
+
+struct FruitDetailView: View {
+    //MARK: PROPERTIES
+    var fruit: Fruit
+        
+    //MARK: BODY
+    var body: some View {
+        NavigationView{
+            ScrollView{
+                VStack(alignment: .center, spacing: 20){
+                    //HEADER
+                    FruitHeaderView(fruit: fruit)
+                    
+                    VStack(alignment: .leading, spacing: 20){
+                       // TITLE
+                       Text(fruit.title)
+                           .font(.largeTitle)
+                           .fontWeight(.heavy)
+                           .foregroundColor(fruit.gradientColors[1])
+                       
+                       //HEADLINE
+                       Text(fruit.headline)
+                           .font(.headline)
+                           .multilineTextAlignment(.leading)
+                       
+                       //NUTRIENTS
+        
+                       //SUBHEADLINE
+                       Text("Learn more about \(fruit.title)".uppercased())
+                           .fontWeight(.bold)
+                           .foregroundColor(fruit.gradientColors[1])
+                       
+                       //DESCRIPTION
+                       Text(fruit.description)
+                           .multilineTextAlignment(.leading)
+                       
+                        //LINK
+                        SourceLinkView()
+                            .padding(.top, 10)
+                            .padding(.bottom, 40)
+                    }//: VSTACK
+                   .padding()
+                   .frame(maxWidth: 640, alignment: .center)
+                }//: VSTACK
+                .navigationBarTitle(fruit.title, displayMode: .inline)
+                .navigationBarHidden(true)
+            }//: SCROLLVIEW
+            .edgesIgnoringSafeArea(.top)
+        }//: NAVIGATION
+    }
+}
+
+//MARK: PREVIEW
+struct FruitDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitDetailView(fruit: fruitsData[0])
+    }
+}
